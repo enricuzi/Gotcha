e03a2b2b34b530152e0f7358fa6223c36d871a9c
enricuzi <luzi.enrico.git@gmail.com>
2015-03-15 11:47:36 +0100

    Node cosole log

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ app.use(express.static('game'));
 
 var server = app.listen(8000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var port = server.address().port;
 
-  console.log('Server started at http://%s:%s', host, port)
+  console.log('Server started at http://localhost:%s', port);
 
 });
